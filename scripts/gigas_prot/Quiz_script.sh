bdc7bb8297f6772381e9fdb9b483ab74fb98df6c
ellior2 <rhondae@taylorshellfish.com>
2016-12-01 18:37:22 -0800

    update

--- scripts/gigas_prot/Quiz_script.sh
+++ scripts/gigas_prot/Quiz_script.sh
@@ -8,6 +8,10 @@ set -u #aborts the shell script if a variable's value has not been set
 
 set -o pipefail #if any program returns a nonzero exit status in the pipe it will cause the entire pipe to return a nonzero status
 
+#change directory
+echo change working directory
+cd C:/Users/rhondae/Documents/Github/Fish-546-Bioinformatics/analyses/taylor
+
 #set variables
 echo set variables
 directoryname="Galaxy_screenshots"
@@ -23,7 +27,6 @@ fi #finished
 mkdir "$directoryname"
 echo confirms creation of directory
 
-ls
 echo listing files in current directory
-
+ls
 
