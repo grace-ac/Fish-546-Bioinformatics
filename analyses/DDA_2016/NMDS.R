937c08d6685c9934b349fd99bf761b0455c5f63f
ellior2 <rhondae@taylorshellfish.com>
2017-04-09 13:32:02 -0700

    update

--- analyses/DDA_2016/NMDS.R
+++ analyses/DDA_2016/NMDS.R
@@ -1,7 +1,7 @@
 install.packages("vegan")
 library(vegan)
 install.packages("raster")
-libary(raster)
+library(raster)
 install.packages("RColorBrewer")
 library(RColorBrewer)
 
@@ -9,7 +9,7 @@ I used the GUI interface, looked on Packages Tab to find Install Packages. Then 
 Then I loaded the BioStatR package.
 
 
-cg.reps<-read.csv('/Users/rhondae/Desktop/2016DDA/Abacus_3_5_17/ABACUS_ADJNSAF_NMDS.csv',header=T,row.names=1)
+cg.reps<-read.csv('/Users/rhondae/Documents/Github/Fish-546-Bioinformatics/analyses/DDA_2016/ABACUS_ADJNSAF_NMDS.csv',header=T,row.names=1)
 
 pool0<-cbind(cg.reps[1],cg.reps[2])
 S2CD3<-cbind(cg.reps[3],cg.reps[4])
@@ -83,5 +83,12 @@ fig.reps<-ordiplot(reps.nmds, choices=c(1,2), type='text', display='sites', xlab
 #assign colors to reps
 points(fig.reps, 'sites', col=c(rep('#E5F5E0',2), rep('#EFF3FF',2), rep('#FEEDDE',2), rep('#F2F0F7',2), rep('#C6DBEF',2), rep('#FDD0A2',2), rep('#DADAEB',2), rep('#9ECAE1',2), rep('#FDAE6B',2), rep('#BCBDDC',2), rep('#6BAED6',2), rep('#FD8D3C',2), rep('#9E9AC8',2), rep('#4292C6',2), rep('#F16913',2), rep('#807DBA',2), rep('#2171B5',2), rep('#D94801',2), rep('#6A51A3',2), rep('#084594',2), rep('#8C2D04',2), rep('#4A1486',2)))
 
+fig.reps
 
-
+reps.nmds<-metaMDS(reps.tra, distance='bray', k=2, trymax=100, autotransform=F)
+fig.reps<-ordiplot(reps.nmds, choices=c(1,2), type='none', display='sites', xlab='Axis 1', ylab='Axis 2', cex=0.5)
+points(fig.reps, 'sites', col=c(rep('black',2), rep('red',2), rep('red',2), rep('red',2), rep('orange',2), rep('orange',2), rep('orange',2),rep('yellow',2), rep('yellow',2), rep('yellow',2), rep('green',2),rep('green',2),rep('green',2), rep('blue',2),rep('blue',2),rep('blue',2),rep('darkslateblue',2),rep('darkslateblue',2),rep('darkslateblue',2),rep('purple',2),rep('purple',2),rep('purple',2)), pch=c(rep(18,2), rep(19,2), rep(15,2), rep(17,2), rep(19,2), rep(15,2),rep(17,2), rep(19,2), rep(15,2), rep(17,2), rep(19,2), rep(15,2), rep(17,2), rep(19,2), rep(15,2), rep(17,2),rep(19,2), rep(15,2), rep(17,2), rep(19,2), rep(15,2), rep(17,2)))
+legend("topright", legend=c("pool0","23C-Silo2", "23C-Silo3", "29C-Silo9"), pch=c(18,19,15,17))
+                                                                                                                                                                                
+#Day 0=black, Day 3=red, Day 5=orange, Day 7=yellow, Day 9=green, Day 11=blue, Day 13=indigo, Day 15=purple
+#pool0= diamonds, 23C-Silo 2 = circles, 23C-Silo 3= square, 29-Silo 9 = triangles
